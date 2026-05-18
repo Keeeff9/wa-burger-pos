2d9095e7b4a86ba17f28d4dcfefa34746e717a28
Deybi Montenegro <deibymontenegrob@gmail.com>
2026-05-18 17:51:28 -0500

    New Styles for the menu added

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("menu/", include("menu.urls")),
-    path("login/", include("login.urls")),
+    path("menu/", include(("menu.urls", 'menu'), namespace='menu')),
+    path("login/", include(("login.urls", 'login'), namespace='login')),
 ]
